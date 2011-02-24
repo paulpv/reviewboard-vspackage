6779f9c5a10ab72113e646e45702b566ca4c07b5
pv@swooby.com <pv@swooby.com@d8ab0883-58c1-7457-caae-3cf82f878979>
2011-02-24 09:45:24 +0000

    Holy crap! This WiX shit is horrible! In this checkin the WiX installer still doesn't work. But, I do have a working VS2008 and VS2010 package being generated from the same code. To run the package in VS2008 you need to run the VS2008 solution. To run the package in VS2010 you need to run the VS2010 solution. Currently I cannot find a way to compile the VS2008 package in VS2010 and have it load in VS2008. I think this has something to do with the VS2010 solution compiling against .Net4, which VS2008 cannot handle. I will see if I can find a way to get VS2010 to compile the VS2008 solution using .Net3.5.

--- trunk/ReviewBoardVsx/Properties/AssemblyInfo.cs
+++ trunk/ReviewBoardVsx/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using ReviewBoardVsx;
+using ReviewBoardVsx.Ids;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
