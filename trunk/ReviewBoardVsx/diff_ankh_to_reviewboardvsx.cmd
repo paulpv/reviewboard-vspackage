44a88a8a20bea01e7b89cec50a5aea3fe550e270
paul.peavyhouse@gmail.com <paul.peavyhouse@gmail.com@d8ab0883-58c1-7457-caae-3cf82f878979>
2011-02-28 09:39:17 +0000

    v0.1.0.1 code base

--- trunk/ReviewBoardVsx/diff_ankh_to_reviewboardvsx.cmd
+++ trunk/ReviewBoardVsx/diff_ankh_to_reviewboardvsx.cmd
@@ -9,35 +9,11 @@ if EXIST %OUTFILE% del %OUTFILE%
 
 set ANKH_ROOT=C:\srcs\ankhsvn\src
 
-set ROOT_LEFT=%ANKH_ROOT%
-set ROOT_RIGHT=.
 
-echo %ROOT_LEFT%\Ankh.Package\Ankh.Package.csproj %ROOT_RIGHT%\ReviewBoardVsx.Package\ReviewBoardVsx.Package.csproj >>%OUTFILE%
+rem AnkhSvn.20XX.sln and ReviewBoardVsx.20XX.sln are so different that there is no need to compare them
 
 
-set ROOT_LEFT=%ANKH_ROOT%\Ankh.UI\VSSelectionControls
-set ROOT_RIGHT=.\ReviewBoardVsx.Package\Ankh\Ankh.UI\VSSelectionControls
-for %%i in (%ROOT_RIGHT%\*.*) do (
-  rem echo i=%%i
-  echo %ROOT_LEFT%\%%~nxi %%i>>%OUTFILE%
-)
-
-
-set ROOT_LEFT=%ANKH_ROOT%\Ankh.Services
-set ROOT_RIGHT=.\ReviewBoardVsx.Package\Ankh\Ankh.Services
-for %%i in (%ROOT_RIGHT%\*.*) do (
-  rem echo i=%%i
-  echo %ROOT_LEFT%\%%~nxi %%i>>%OUTFILE%
-)
-
-
-set ROOT_LEFT=%ANKH_ROOT%\tools\Ankh.GenerateVSIXManifest
-set ROOT_RIGHT=.\tools\ReviewBoardVsx.GenerateVSIXManifest
-echo %ROOT_LEFT%\Program.cs                       %ROOT_RIGHT%\Program.cs                                 >>%OUTFILE%
-echo %ROOT_LEFT%\Ankh.GenerateVSIXManifest.csproj %ROOT_RIGHT%\ReviewBoardVsx.GenerateVSIXManifest.csproj >>%OUTFILE%
-echo %ROOT_LEFT%\Properties\AssemblyInfo.cs       %ROOT_RIGHT%\Properties\AssemblyInfo.cs                 >>%OUTFILE%
-
-
+:ankh_genpkgdef
 set ROOT_LEFT=%ANKH_ROOT%\tools\Ankh.GenPkgDef
 set ROOT_RIGHT=.\tools\Ankh.GenPkgDef
 for %%i in (%ROOT_RIGHT%\*.*) do (
@@ -47,6 +23,35 @@ for %%i in (%ROOT_RIGHT%\*.*) do (
 for %%i in (%ROOT_RIGHT%\Properties\*.*) do (
   rem echo i=%%i
   echo %ROOT_LEFT%\Properties\%%~nxi %%i>>%OUTFILE%
+)
+
+
+:ankh_generatevsixmanifest
+set ROOT_LEFT=%ANKH_ROOT%\tools\Ankh.GenerateVSIXManifest
+set ROOT_RIGHT=.\tools\ReviewBoardVsx.GenerateVSIXManifest
+echo %ROOT_LEFT%\Program.cs                       %ROOT_RIGHT%\Program.cs                                 >>%OUTFILE%
+echo %ROOT_LEFT%\Ankh.GenerateVSIXManifest.csproj %ROOT_RIGHT%\ReviewBoardVsx.GenerateVSIXManifest.csproj >>%OUTFILE%
+echo %ROOT_LEFT%\Properties\AssemblyInfo.cs       %ROOT_RIGHT%\Properties\AssemblyInfo.cs                 >>%OUTFILE%
+
+
+rem AnkhSvn.Ids and ReviewBoardVsx.Ids are so different that there is no need to compare them
+
+
+:ankh_package
+set ROOT_LEFT=%ANKH_ROOT%\Ankh.Package
+set ROOT_RIGHT=.\ReviewBoardVsx.Package
+echo %ROOT_LEFT%\Ankh.Package.csproj %ROOT_RIGHT%\ReviewBoardVsx.Package.csproj >>%OUTFILE%
+
+set ROOT_LEFT=%ANKH_ROOT%\Ankh.UI\VSSelectionControls
+for %%i in (%ROOT_RIGHT%\Ankh\Ankh.UI\VSSelectionControls\*.*) do (
+  rem echo i=%%i
+  echo %ROOT_LEFT%\%%~nxi %%i>>%OUTFILE%
+)
+
+set ROOT_LEFT=%ANKH_ROOT%\Ankh.Services
+for %%i in (%ROOT_RIGHT%\Ankh\Ankh.Services\*.*) do (
+  rem echo i=%%i
+  echo %ROOT_LEFT%\%%~nxi %%i>>%OUTFILE%
 )
 
 
